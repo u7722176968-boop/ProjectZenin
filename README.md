5b4560aef07617047240aeeac93859da37ce1172
u7722176968-boop <u7722176968@gmail.com>
2026-09-16 12:51:51 +0000

    stapel.py soweit fertig, mit ein paar Tests aber erste Prototyp funktioniert

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ProjectZenin
 kleines Uno Spiel für den Informatik-Unterricht 4er Gruppe
+
+Spielreihenfolge mit Befehlen:
+1. Karten erzeugen mit ErzeugeKarten()
+2. Karten mischen durch mischen() anfänglich mit dem Attribut 0
+3. Karten auf Hände verteilen (noch nicht implementiert)
+4. Karte aufdecken mit aufdecken(), hierbei wird die erste Karte aus verdeckt genommen und zu der letzten Karte von abgelegt
+5. Spiel ansich startet
+
+
+Info: getAnzahl(), 0 -> verdeckter Stappel, andere Zahl -> abgelegter Stappel
+
+To-Dos für später:
+- Befehle nicht immer ausführbar machen, z.B. kartenausgeben oder aufdecken nur am Anfang
--- stapel.py
+++ stapel.py
@@ -1,21 +1,23 @@
 from karte import Karte
+from hand import Hand
+import random
 class Stapel:
     def __init__(self):
         self.verdeckt = []
         self.abgelegt = []
     def StapelErzeugen(self):
         for i in range(2):
-            for i in range(10):
-                karte = Karte("blau", i)
+            for i in range(9):
+                karte = Karte("blau", i+1)
                 self.verdeckt.append(karte)
-            for i in range(10):
-                karte = Karte("grün", i)
+            for i in range(9):
+                karte = Karte("grün", i+1)
                 self.verdeckt.append(karte)
-            for i in range(10):
-                karte = Karte("rot", i)
+            for i in range(9):
+                karte = Karte("rot", i+1)
                 self.verdeckt.append(karte)
-            for i in range(10):
-                karte = Karte("gelb", i)
+            for i in range(9):
+                karte = Karte("gelb", i+1)
                 self.verdeckt.append(karte)
         karteblau = Karte("blau", 0)
         kartegrün = Karte("grün", 0)
@@ -26,19 +28,53 @@ class Stapel:
         self.verdeckt.append(karterot)
         self.verdeckt.append(kartegelb)
     def getVerdeckt(self):
-        return self.verdeckt
+        return self.verdeckt  
     def getAbgelegt(self):
         return self.abgelegt
-    def setVerdeckt(self, karten):
-        self.verdeckt = karten
-    def setAbgelegt(self, karten):
-        self.abgelegt = karten
+    def setVerdeckt(self, karte):
+        self.verdeckt.append(karte)
+    def setAbgelegt(self, karte):
+        self.abgelegt.append(karte)
     def verdecktToString(self):
         text = "verdeckt:\n"
         for karte in self.verdeckt:
             text += str(karte) + "\n"
         print(text)
-
+    def abgelegtToString(self):
+            text = "abgelegt:\n"
+            for karte in self.abgelegt:
+                text += str(karte) + "\n"
+            print(text)
+    def mischen(self, stapelnummer):
+        if stapelnummer == 0:
+            random.shuffle(self.verdeckt)
+        else:
+            random.shuffle(self.abgelegt)
+    def aufdecken(self):
+        aufgedeckteKarte = self.verdeckt.pop(0)
+        self.abgelegt.append(aufgedeckteKarte)
+    def getAnzahl(self, nummer):
+        if nummer == 0:
+            return len(self.verdeckt)
+        else:
+            return len(self.abgelegt)
+    def ausgeben(self, spieleranzahl):
+        haende = []
+        for i in range(spieleranzahl):
+            verteilteKarten = []
+            for _ in range(7):
+                karteVerteilung = self.verdeckt.pop(0)
+                verteilteKarten.append(karteVerteilung)
+            anfangsHand = Hand(verteilteKarten, i+1)
+            haende.append(anfangsHand)
+        for k in haende:
+            print(k)
+        return haende
+        
 stapel1 = Stapel()
 stapel1.StapelErzeugen()
 stapel1.verdecktToString()
+stapel1.mischen(0)
+stapel1.verdecktToString()
+stapel1.ausgeben(2)
+print(str(len(stapel1.getVerdeckt())))
