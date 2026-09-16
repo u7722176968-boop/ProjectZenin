4d173f3230901946f647cf3e191d55e33f8212f3
u7722176968-boop <u7722176968@gmail.com>
2026-09-16 09:15:38 +0000

    spiel.py lässt SpielerAnzahl entern

--- spiel.py
+++ spiel.py
@@ -1,7 +1,20 @@
 from hand import Hand
-from stapel import Stapel
+#from stapel import Stapel
+
 
 class Spiel:
     def __init__(self):
-            self.Stapel = Stapel
-            self.spieler = spieler
+            #self.Stapel = Stapel
+            self.spieler = []
+    def spielen(self):
+            print("Spieleranzahl eingeben: ")
+            i = input()
+            for x in range(int(i)):
+                hand = Hand([], x+1)
+                self.spieler.append(hand)
+            print(self.spieler)
+Spiel = Spiel()
+Spiel.spielen()
+        
+        
+        
