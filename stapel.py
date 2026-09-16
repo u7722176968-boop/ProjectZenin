1b44316c199830fb3dd6d500c8def1af202d9818
u7722176968-boop <u7722176968@gmail.com>
2026-09-16 11:19:20 +0000

    Stapel verändert Mara

--- stapel.py
+++ stapel.py
@@ -1,54 +1,41 @@
-import java.util.ArrayList;
-import java.util.Collections;
+from karte import Karte
+class Stapel:
+    def __init__(self):
+        self.verdeckt = []
+        self.abgelegt = []
+    def StapelErzeugen(self):
+        for i in range(2):
+            for i in range(10):
+                karte = Karte("blau", i)
+                self.verdeckt.append(karte)
+            for i in range(10):
+                karte = Karte("grün", i)
+                self.verdeckt.append(karte)
+            for i in range(10):
+                karte = Karte("rot", i)
+                self.verdeckt.append(karte)
+            for i in range(10):
+                karte = Karte("gelb", i)
+                self.verdeckt.append(karte)
+    def getVerdeckt(self):
+        return self.verdeckt
+    def getAbgelegt(self):
+        return self.abgelegt
+    def setVerdeckt(self, karten):
+        self.verdeckt = karten
+    def setAbgelegt(self, karten):
+        self.abgelegt = karten
+    def verdecktToString(self):
+        text = "verdeckt:\n"
+        for karte in self.verdeckt:
+            text += str(karte) + "\n"
+        print(text)
 
-public class Stapel {
+stapel1 = Stapel()
+karte1 = Karte("blau", 0)
+karte2 = Karte("rot", 6)
+karte3 = Karte("grün", 4)
 
-    private ArrayList<Karte> karten;
-
-    // Erstellt einen neuen Stapel
-    public Stapel() {
-        karten = new ArrayList<>();
-        erstelleStapel();
-        mischen();
-    }
-
-    // UNO-Karten erstellen
-    private void erstelleStapel() {
-        String[] farben = {"Rot", "Gelb", "Grün", "Blau"};
-
-        for (String farbe : farben) {
-            // 0 gibt es einmal
-            karten.add(new Karte(farbe, 0));
-
-            // Zahlen 1-9 gibt es zweimal
-            for (int i = 1; i <= 9; i++) {
-                karten.add(new Karte(farbe, i));
-                karten.add(new Karte(farbe, i));
-            }
-        }
-    }
-
-    // Karten mischen
-    public void mischen() {
-        Collections.shuffle(karten);
-    }
-
-    // Oberste Karte ziehen
-    public Karte ziehen() {
-        if (karten.isEmpty()) {
-            return null;
-        }
-
-        return karten.remove(karten.size() - 1);
-    }
-
-    // Anzahl der Karten im Stapel
-    public int anzahlKarten() {
-        return karten.size();
-    }
-
-    // Prüfen, ob der Stapel leer ist
-    public boolean istLeer() {
-        return karten.isEmpty();
-    }
-}
+karten = [karte1, karte2, karte3]
+stapel1.setVerdeckt(karten)
+stapel1.verdecktToString()
